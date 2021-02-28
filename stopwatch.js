0135e71f83527b193b7a99cd618e370b1aaa7931
182Marco <marcomilza@gmail.com>
2021-02-28 01:47:12 +0100

    imitazione layout apple

--- stopwatch.js
+++ stopwatch.js
@@ -1,7 +1,7 @@
 const countDownElement = document.querySelector('#timer');
 const buttonStart = document.querySelector('#startStop');
-const buttonStop = document.querySelector('#reset');
-const buttonlap = document.querySelector('#lap');
+const buttonGiroStop = document.querySelector('#reset');
+const btnWrapperStop = document.querySelector('#btnWrapperStop');
 
 buttonStart.addEventListener(`click`, startStop);
 
@@ -29,28 +29,36 @@ function startStop() {
       minutes = minutes < 10 ? `0` + minutes : minutes;
       hours = hours < 10 ? `0` + hours : hours;
       //SCRIVERE DENTRO NUOVO MINUTAGGIO
-      countDownElement.innerHTML = `${hours}:${minutes}:${seconds}:${cent}`;
+      countDownElement.innerHTML = `${hours}:${minutes}:${seconds},${cent}`;
       allCentInCount++;
       //CAMBIARE STATO AL BOTTONE START PER
       //RIUSARLO COME STOP NELL'ELSE IF
       buttonStart.innerText = 'stop';
-      buttonStart.style.backgroundColor = '#a2120e';
+      buttonStart.style.backgroundColor = '#330e0c';
+      buttonStart.style.color = '#ff453a';
       buttonStart.setAttribute('status', 'stop');
+      btnWrapperStop.style.backgroundColor = '#330e0c';
     }
   } else if (buttonStart.getAttribute('status') == 'stop') {
     clearInterval(int);
     buttonStart.setAttribute('status', 'readytoreset');
+    //cambiare nome al tasto "Giro"
+    buttonGiroStop.innerText = 'Azzera';
   }
 }
 
-buttonStop.addEventListener(`click`, reset);
+buttonGiroStop.addEventListener(`click`, reset);
 function reset() {
   if (buttonStart.getAttribute('status') == 'readytoreset') {
     clearInterval(int);
     countDownElement.innerHTML = `00:00:00:00`;
     buttonStart.setAttribute('status', 'basic');
-    buttonStart.innerText = 'start';
-    buttonStart.style.backgroundColor = 'green';
+    buttonStart.innerText = 'Avvia';
+    buttonStart.style.backgroundColor = '#0a2a12';
+    buttonStart.style.color = '#30d158';
+    btnWrapperStop.style.backgroundColor = '#0a2a12';
+    //cambiare nome al tasto "Azzera"
+    buttonGiroStop.innerText = 'Giro';
     var allLapCont = document.querySelectorAll('.SingolLapCont');
     var i;
     for (i = 0; i < allLapCont.length; i++) {
@@ -59,10 +67,10 @@ function reset() {
   }
 }
 
-buttonlap.addEventListener(`click`, lap);
+buttonGiroStop.addEventListener(`click`, lap);
 function lap() {
   if (
-    countDownElement.innerText != '00:00:00:00' &&
+    countDownElement.innerText != '00:00:00,00' &&
     buttonStart.getAttribute('status') != 'readytoreset'
   ) {
     //costruire un div per contenere N lap e lap
@@ -83,7 +91,7 @@ function lap() {
     nlap = document.querySelectorAll('.lap').length;
     //creare il relativo elemento
     var lapEl = document.createElement('span');
-    var textLap = document.createTextNode(`lap ${nlap}`);
+    var textLap = document.createTextNode(`Giro ${nlap}`);
     lapEl.appendChild(textLap);
     singolLapCont.appendChild(lapEl);
     timeLap.classList.add('Nlap');
